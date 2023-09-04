5229f95cee9244257bae20ed40c42088add93193
Miguel Castillo <castillo@Miguels-MBP.attlocal.net>
2023-09-03 19:02:08 -0500

    Works but does not display recorded video on 3rd screen.

--- app/src/main/java/com/example/smarthomegesturecontrolapplication_part1_miguelcastillo/SecondScreenActivity.java
+++ app/src/main/java/com/example/smarthomegesturecontrolapplication_part1_miguelcastillo/SecondScreenActivity.java
@@ -18,11 +18,11 @@ import android.view.View;
 import androidx.annotation.Nullable;
 
 public class SecondScreenActivity extends AppCompatActivity {
-    String gestureName;
+    String gestureAction;
     String demoFileName;
-    String filenameGesture;
+    String practiceFileNameGesture;
     String demoVideoPath;
-
+    int demoVideoReplayCount = 0;
     VideoView demoVideoView;
     Button practiceButton;
     Button restartButton;
@@ -35,17 +35,9 @@ public class SecondScreenActivity extends AppCompatActivity {
 
         //Get user choice from dropdown
         Intent intent = getIntent();
-        gestureName = intent.getStringExtra("Gesture Name");
-        videoAndFileName(gestureName);
+        gestureAction = intent.getStringExtra("Gesture Name");
 
-        //Set the demo video on gui
-//        int resourceID = this.getResources().getIdentifier(demoFileName, "raw", this.getPackageName());
-//        //Find the video location inside phone
-//        Uri demoLocation = Uri.parse("android.resource://" + getPackageName() + "/" + resourceID);
-//        demoView.setVideoURI(demoLocation);
-//        demoView.start();
-//        demoView.setOnCompletionListener(mediaPlayer -> demoView.start());
-
+        videoAndFileName(gestureAction);
 
         //Display corresponding demo video
         demoVideoView = findViewById(R.id.demoVideoView);
@@ -53,22 +45,47 @@ public class SecondScreenActivity extends AppCompatActivity {
         demoVideoView.setVideoURI(Uri.parse(demoVideoPath));
         demoVideoView.start();
 
+        //Replay demo video 3 times
+        demoVideoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            int maxReplayCount =3;
+            @Override
+            public void onCompletion(MediaPlayer mediaPlayer) {
+                if(demoVideoReplayCount < maxReplayCount){
+                    demoVideoReplayCount++;
+                    mediaPlayer.seekTo(0);
+                    mediaPlayer.start();
+                }
+            }
+        });
+
         //Buttons
         practiceButton = findViewById(R.id.practiceButtonScreen2);
         practiceButton.setOnClickListener(new View.OnClickListener(){
            @Override
            public void onClick(View v){
             Intent intent = new Intent(SecondScreenActivity.this, ThirdScreenActivity.class);
-            intent.putExtra("Gesture Name", gestureName);
+//            intent.putExtra("Gesture Name", gestureAction);
+            intent.putExtra("Practice Gesture File Name", practiceFileNameGesture);
             startActivity(intent);
 
             }
         });
+//        practiceButton.setOnClickListener(new View.OnClickListener(){
+////            practiceFileName = gestureName + "_PRACTICE_" + practiceNum + userName + ".mp4";
+//            @Override
+//            public void onClick(View v){
+//                Intent intent = new Intent(SecondScreenActivity.this, ThirdScreenActivity.class);
+//                intent.putExtra("Gesture Name", gestureName);
+//                startActivity(intent);
+//
+//            }
+//        });
 
         restartButton = findViewById(R.id.restartButtonScreen2);
         Intent intent2 = new Intent(SecondScreenActivity.this, MainActivity.class);
-
         restartButton.setOnClickListener(v -> startActivity(intent2));
+
+
 
     }
     public void videoAndFileName(String gestureAction){
@@ -76,67 +93,71 @@ public class SecondScreenActivity extends AppCompatActivity {
 
             case "Turn On Light":
                 demoFileName = "lighton";
-                filenameGesture = "LightOn";
+                practiceFileNameGesture = "LightOn";
                 break;
             case "Turn Off Light":
                 demoFileName = "lightoff";
-                filenameGesture = "LightOff";
+                practiceFileNameGesture = "LightOff";
                 break;
             case "Turn On Fan":
                 demoFileName = "fanon";
-                filenameGesture = "FanOn";
+                practiceFileNameGesture = "FanOn";
                 break;
             case "Turn Off Fan":
                 demoFileName = "fanoff";
-                filenameGesture = "FanOff";
+                practiceFileNameGesture = "FanOff";
                 break;
             case "Increase Fan Speed":
                 demoFileName = "increasefanspeed";
-                filenameGesture = "FanUp";
+                practiceFileNameGesture = "FanUp";
                 break;
             case "Decrease Fan Speed":
-                demoFileName = "decreaseFanSpeed";
-                filenameGesture = "FanDown";
+                demoFileName = "decreasefanspeed";
+                practiceFileNameGesture = "FanDown";
                 break;
             case "Set Thermostat to specified temperature":
                 demoFileName = "setthermo";
-                filenameGesture = "SetThermo";
+                practiceFileNameGesture = "SetThermo";
                 break;
             case "0":
                 demoFileName = "h0";
-                filenameGesture = "Num0";
+                practiceFileNameGesture = "Num0";
                 break;
             case "1":
                 demoFileName = "h1";
-                filenameGesture = "Num1";
+                practiceFileNameGesture = "Num1";
                 break;
             case "2":
                 demoFileName = "h2";
-                filenameGesture = "Num2";
+                practiceFileNameGesture = "Num2";
                 break;
             case "3":
                 demoFileName = "h3";
-                filenameGesture = "Num3";
+                practiceFileNameGesture = "Num3";
                 break;
             case "4":
                 demoFileName = "h4";
-                filenameGesture = "Num4";
+                practiceFileNameGesture = "Num4";
                 break;
             case "5":
+                demoFileName = "h5";
+                practiceFileNameGesture = "Num5";
+                break;
+            case "6":
                 demoFileName = "h6";
-                filenameGesture = "Num6";
+                practiceFileNameGesture = "Num6";
                 break;
             case "7":
                 demoFileName = "h7";
-                filenameGesture = "Num0";
+                practiceFileNameGesture = "Num7";
                 break;
             case "8":
                 demoFileName = "h8";
-                filenameGesture = "Num8";
+                practiceFileNameGesture = "Num8";
                 break;
             case "9":
                 demoFileName = "h9";
-                filenameGesture = "Num9";
+                practiceFileNameGesture = "Num9";
                 break;
         }
     }
--- app/src/main/java/com/example/smarthomegesturecontrolapplication_part1_miguelcastillo/ThirdScreenActivity.java
+++ app/src/main/java/com/example/smarthomegesturecontrolapplication_part1_miguelcastillo/ThirdScreenActivity.java
@@ -1,27 +1,57 @@
 package com.example.smarthomegesturecontrolapplication_part1_miguelcastillo;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.content.Intent;
+import android.provider.MediaStore;
 import android.widget.Button;
+import android.media.MediaPlayer;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.File;
+
+import android.os.Environment;
+
+import androidx.core.content.FileProvider;
+
+
 public class ThirdScreenActivity extends AppCompatActivity {
-
-    String gestureName;
-
+    String practiceFileNameGesture;
+    String filenameGesture;
+//    String practiceVideoFileName;
+    String practiceVideoPath;
     Button uploadButton;
     Button restartButton;
     Button practiceButton;
+    String userLastName = "CASTILLO";
+    Uri savedPracticeVideoLocation;
+    VideoView practiceVideoView;
+
+    int option = 1;
     @Override
     public void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
 
+//        Intent gestureName = getIntent();
+//        gestureAction = getIntent().getStringExtra("Gesture Name");
+        practiceFileNameGesture = getIntent().getStringExtra("Practice Gesture File Name");
+
+        String practiceVideoFileName = practiceFileNameGesture + "_PRACTICE_" + userLastName + ".mp4";
+
+        //logic to open camera
+        recordPracticeVideo(practiceVideoFileName);//Block execution
+
+        //Display third screen after recording practice video
         setContentView(R.layout.activity_third_screen);
 
-//        Intent gestureName = getIntent();
-        gestureName = getIntent().getStringExtra("Gesture Name");
-
+        //Play recorded practice video
+        practiceVideoView = findViewById(R.id.practiceVideoView);
+//        practiceVideoPath = "android.resource://" + getPackageName() + "/raw/" + practiceVideoFileName;
+        practiceVideoView.setVideoURI(savedPracticeVideoLocation);
+        practiceVideoView.start();
+        practiceVideoView.setOnCompletionListener(mediaPlayer -> practiceVideoView.start());
 
         //Create buttons
         uploadButton = findViewById(R.id.uploadButton);
@@ -30,5 +60,20 @@ public class ThirdScreenActivity extends AppCompatActivity {
         restartButton = findViewById(R.id.restartButtonScreen3);
         Intent intent = new Intent(ThirdScreenActivity.this,MainActivity.class);
         restartButton.setOnClickListener(v-> startActivity(intent));
+    }
+
+
+    public void recordPracticeVideo(String practiceVideoFileName){
+
+        File practiceFileVideo = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/Videos/"+ practiceVideoFileName );
+
+        Intent recordPracticeVideo = new Intent(MediaStore.ACTION_VIDEO_CAPTURE);
+        recordPracticeVideo.putExtra(MediaStore.EXTRA_DURATION_LIMIT, 5);//Record for max 5 seconds
+
+        //Get file location where is being saved
+//        Uri savedPracticeVideoLocation = Uri.fromFile(practiceFileVideo);
+        savedPracticeVideoLocation = FileProvider.getUriForFile(getApplicationContext(), "com.example.smarthomegesturecontrolapplication_part1_miguelcastillo.provider", practiceFileVideo);
+        recordPracticeVideo.putExtra(MediaStore.EXTRA_OUTPUT, savedPracticeVideoLocation);
+        startActivity(recordPracticeVideo);
     }
 }
